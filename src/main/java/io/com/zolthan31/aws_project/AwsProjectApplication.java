d441a7329cd427060246e8cc2d4ea33d420fce08
zolthan31 <dhb.tecnologia@outlook.com>
2023-06-10 00:04:15 -0300

    criacao do projeto e primeiro endpoint

---
+++ src/main/java/io/com/zolthan31/aws_project/AwsProjectApplication.java
@@ -0,0 +1,13 @@
+package io.com.zolthan31.aws_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/com/zolthan31/aws_project/AwsProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package io.com.zolthan31.aws_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
